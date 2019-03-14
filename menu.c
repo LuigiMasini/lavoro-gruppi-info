aefca1646f1a3bdfca562edcefc104cbe06cd7f9
Luigi Masini <luigi_masini@tiscali.it>
2019-03-14 20:14:53 +0100

    allineamento

--- menu.c
+++ menu.c
@@ -3,57 +3,57 @@
 
 int main()
 {
-    int menu=1,opz,opzb;
-    
-    while(menu==1)
-    {
-        printf("Salve Utente,\n"
+	int menu=1,opz,opzb;
+	
+	while(menu==1)
+	{
+		printf("Salve Utente,\n"
 		"Questo programma permette di creare una figura tra quelle proposte, e di personalizzarla modificandone proporzioni o colore.\n\n"
 		"Ecco le opzioni:\n\n"
 		"F1 F2 F3 F4 F5 menu bonus(6) credits(7)\n\n"
 		"Inserire il numero dell'opzione desiderata");
-        scanf("%d",&opz);
-        while(opz>7||opz<1)
-        {
-            printf("Si prega di inserire uno dei numeri corrispondenti alle differenti opzioni, sopra riportati");
-         }
-         switch (opz){
-             case 1:
-                    break;
-             case 2:
-                    break;
-             case 3:
-                    break;
-             case 4:
-                    break;
-             case 5:
-                    break;
-             case 6:menu==2;
-                    while(menu==2)
-                    {
-                        printf("BENVENUTO NEL MENU BONUS: -va bene, tieniti pure i tuoi segreti\nSelezionare un opzione: OPZ1 OPZ2 ECC.. MENU BASE3");
-                        scanf("%d",&opzb);
-                        while(opzb<0||opzb>3)
-                        {printf("\ninserire un opzione valida");
-                                scanf("%d",&opzb);
-                        }
-                        switch (opzb){
-                            case 1:
-                                   break;
-                            case 2:
-                                   break;
-                            case 3:menu==1;
-                            printf("-nothing strange here");
-                                   break;
-                    }
-                    break;
-             case 7:printf("\n*Programmatori: Masini Luigi, Negro Federico\nSuddivisione del lavoro:\nMasini:\nNegro:\nCollab:\n");
-                    break;
-         }
-    }
-        printf("\n\n\n\n");
-    }
-    return 0;
+		scanf("%d",&opz);
+		while(opz>7||opz<1)
+		{
+			printf("Si prega di inserire uno dei numeri corrispondenti alle differenti opzioni, sopra riportati");
+		}
+		switch (opz){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:menu==2;
+			while(menu==2)
+			{
+				printf("BENVENUTO NEL MENU BONUS: -va bene, tieniti pure i tuoi segreti\nSelezionare un opzione: OPZ1 OPZ2 ECC.. MENU BASE3");
+				scanf("%d",&opzb);
+				while(opzb<0||opzb>3)
+				{printf("\ninserire un opzione valida");
+					scanf("%d",&opzb);
+				}
+				switch (opzb){
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:menu==1;
+					printf("-nothing strange here");
+					break;
+				}
+				break;
+					case 7:printf("\n*Programmatori: Masini Luigi, Negro Federico\nSuddivisione del lavoro:\nMasini:\nNegro:\nCollab:\n");
+					break;
+			}
+		}
+		printf("\n\n\n\n");
+	}
+	return 0;
 }
 
 
