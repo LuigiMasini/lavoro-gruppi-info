16eaccb9be6926db6bb3f90e0b1d9abafe1b6efa
Luigi Masini <luigi_masini@tiscali.it>
2019-03-23 00:20:24 +0100

    mminor  changes

--- coloreunix.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main(){
-	printf("\033[1;31mbold red text\033[0m\n");
-	
-}
--- omino.c
+++ omino.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <wait.h>
+
+// #define debug
 
 int main(){
 	
-	char *cls = "clear";
+	
 	char *omi[] = {
 		
 		//1, 0
@@ -185,19 +188,59 @@ int main(){
 		
 	};
 	
+	//u can't touch this, MC Hammer,		134 bpm
+	//live in the moment, portugal + the man,	128 bpm
 	
-	for(int i =0; 1; i==15? i=0 : i++){
-		system(cls);
-		printf("%d\n%s", i, omi[i]);
-// 		sleep(1);	//in seconds
-		usleep(100000);	//in microseconds
-// 		getchar();
+	int bpm = 134;			//beat per minute
+	double d = 7.262041;		//duration in seconds
+	int fpb = 2;			//frame per beat
+	
+	int fpm = fpb*bpm;
+	double step_s = 60;
+	step_s/=fpm;
+	double step_ms = step_s*1e6;	//step in microseconds
+	double cont = d;
+	cont/=step_s;
+	
+#ifdef debug
+	printf("bpm\t\t%d\nduration\t%f\nfpb\t\t%d\nfpm\t\t%d\nstep s\t\t%f\nstep ms\t\t%f\nframe tot\t%f\n\n", bpm, d, fpb, fpm, step_s, step_ms, cont);	//debug
+	fflush(stdout);
+#endif
+	
+	pid_t pid = fork();
+	if ( pid == 0 ){
+		int ret = system("./linux_play cant_touch_this.mp3");
+		exit(WEXITSTATUS(ret));
+	}
+	else {
+#ifdef debug
+		printf("%d", (int)cont);
+#endif
+		usleep(step_ms/2);
+		int i=0;
+		while((int)cont--){
+			system("clear");
+#ifdef debug
+			printf("%d %d\n", i, (int)cont);
+#endif
+			printf("%s", omi[i]);
+			fflush( stdout );
+			usleep((int)step_ms);	//in microseconds
+			i==15 ? i=0: i++ ;
+		}
+		
+		int status;
+		waitpid(pid, &status, 0);
+#ifdef debug
+		printf("Exit status of the child was %d\n", WEXITSTATUS(status));
+#endif
 	}
 	
 }
 
 /*TODO:
-molto inefficente con array tridimensionali, meglio perndere sigoli frame da un file dedicato
-aggiungere musica e adattare tempo
-aggiungere colori
-*/
+ m o*lto inefficente con array tridimensionali, meglio perndere sigoli frame da un file dedicato
+-aggiungere musica e adattare tempo
+ aggiungere colori
+ aggiungere compilazione sbs step by step per controllo fotogrammi uno ad uno
+ */
